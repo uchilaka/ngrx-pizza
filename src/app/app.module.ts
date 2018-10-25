b2b0136b362b32c38dbc04d0a5f8f67f7864bd36
Uchenna Chilaka <uchenna.chilaka@alliancedata.com>
2018-10-25 17:08:47 -0400

    first tutorial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { StoreModule, MetaReducer } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+import { StoreModule, MetaReducer } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
 
 // not used in production
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { storeFreeze } from 'ngrx-store-freeze';
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { storeFreeze } from "ngrx-store-freeze";
 
 // this would be done dynamically with webpack for builds
 const environment = {
   development: true,
-  production: false,
+  production: false
 };
 
+// Applied to every single reducer
 export const metaReducers: MetaReducer<any>[] = !environment.production
   ? [storeFreeze]
   : [];
 
 // bootstrap
-import { AppComponent } from './containers/app/app.component';
+import { AppComponent } from "./containers/app/app.component";
 
 // routes
 export const ROUTES: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: "", pathMatch: "full", redirectTo: "products" },
   {
-    path: 'products',
-    loadChildren: '../products/products.module#ProductsModule',
-  },
+    path: "products",
+    loadChildren: "../products/products.module#ProductsModule"
+  }
 ];
 
 @NgModule({
@@ -39,9 +40,9 @@ export const ROUTES: Routes = [
     RouterModule.forRoot(ROUTES),
     StoreModule.forRoot({}, { metaReducers }),
     EffectsModule.forRoot([]),
-    environment.development ? StoreDevtoolsModule.instrument() : [],
+    environment.development ? StoreDevtoolsModule.instrument() : []
   ],
   declarations: [AppComponent],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
